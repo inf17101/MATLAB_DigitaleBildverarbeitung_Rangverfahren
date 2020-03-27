48ac57b10c344b852093ced80878293d449d12a0
Oliver Klapper <oliver.klapper@web.de>
2020-03-27 10:14:21 +0100

    adds periodic continous padding

--- Rangverfahren.m
+++ Rangverfahren.m
@@ -1,14 +1,38 @@
-function [InputPicture,SobelMatrix, GradientenMatrix] = Rangverfahren(input_file, index, threshold_rang, threshold_sobel, auto_threshold_activated)
+function [InputPicture,SobelMatrix, RangMatrix] = Rangverfahren(input_file, index, threshold_rang, threshold_sobel, auto_threshold_activated)
 
-if index == '1'
+InputPicture = imread(input_file);
+[~, ~, dim3] = size(InputPicture);
+
+if dim3 == 3
+    InputPicture = rgb2gray(InputPicture);
+end
+
+InputPicture = double(InputPicture); % cast to double matrix
+%InputPicture = padarray(InputPicture, padding, 0); % insert padding of zeros around the matrix
+
+
+Sobel = fspecial('sobel');
+SX = imfilter(InputPicture,Sobel);
+SY = imfilter(InputPicture',Sobel)';
+GradientenMatrix = sqrt(SX.*SX + SY.*SY);
+SobelMatrix = (GradientenMatrix > threshold_sobel);
+
+[r, c] = size(GradientenMatrix);
+I_Periodic = [GradientenMatrix GradientenMatrix GradientenMatrix; GradientenMatrix GradientenMatrix GradientenMatrix; GradientenMatrix GradientenMatrix GradientenMatrix;]; % periodic continous picture
+[rPeriodic, cPeriodic] = size(I_Periodic);
+
+if index == '1' % 3x3 surrounding
     stepwith = 2;
-    padding = [1 1]; % inserting 1 line of zeros around matrix
-elseif index == '2'
+    padding = 1; % 1 line to remove after periodic continous with 3x3
+    GradientenMatrix = I_Periodic(r:rPeriodic-r+1,c:cPeriodic-c+1);
+elseif index == '2' %5x5 surrounding
     stepwith = 4;
-    padding = [2 2]; % inserting 2 lines of zeros around matrix
-else
+    padding = 2; % 2 lines to remove after periodic continous with 5x5
+    GradientenMatrix = I_Periodic(r-1:rPeriodic-r+2,c-1:cPeriodic-c+2);
+else % 7x7 surrounding
     stepwith = 6;
-    padding = [3 3]; % inserting 3 lines of zeros around matrix
+    padding = 3; % 3 lines to remove after periodic continous with 7x7
+    GradientenMatrix = I_Periodic(r-2:rPeriodic-r+3,c-2:cPeriodic-c+3);
 end
 
 if auto_threshold_activated == 1
@@ -16,64 +40,26 @@ if auto_threshold_activated == 1
     threshold_rang = 0.5 * (nxn_dim * nxn_dim + 1);
 end
 
-%InputPicture = imread('Testbild_Kreis_100x100.png');
-%InputPicture = imread('Testbild_Fuchs.png');
-%[X,Y] = meshgrid([-2:0.01:2],[-2:0.01:2]);
-%C = sqrt(X.*X + Y.*Y);
-%E = 255./(1+exp((C-1)/0.05));
-%InputPicture = (1+ 0.9*X).*E;
-%InputPicture = imread('SternPic.png');
-%InputPicture = imread('6EckPic.png');
-%InputPicture = imread('Xpic600px.png');
-%InputPicture = imread('PicTest600px.png');
-%InputPicture = imread('ManyForms200px.png');
-%InputPicture = imread('ManyForms600px.png');
-%InputPicture = imread('Kreisfarbverlauf800px.png');
-%InputPicture = imread('Kreisfarbverlauf2_800px.png');
-%InputPicture = imread('SelbstmalKreis800px.png');
-%InputPicture = imread('Kreis_NoSmooth20px.png');
-%InputPicture = imread('GimpPicture.png');
-%InputPicture = imread('ZugPic.png');
-%InputPicture = imread('Gimp2.png');
-InputPicture = imread(input_file);
-[~, ~, dim3] = size(InputPicture);
-
-if dim3 == 3
-    InputPicture = rgb2gray(InputPicture);
-end
-InputPicture = padarray(InputPicture, padding, 0); % insert padding of zeros around the matrix
-InputPicture = double(InputPicture);
-
-Sobel = fspecial('sobel');
-SX = imfilter(InputPicture,Sobel);
-SY = imfilter(InputPicture',Sobel)';
-I = sqrt(SX.*SX + SY.*SY);
-SobelMatrix = (I > threshold_sobel);
-I2 = I;
-GradientenMatrix = I;
-
-[r, c] = size(I);
-
+I2 = GradientenMatrix;
+RangMatrix = GradientenMatrix;
 for q1=1:(r-stepwith)
     for p1=1:(c-stepwith)
-        I = I2(q1:(q1+stepwith), p1:(p1+stepwith)); % nxn Umgebung %
-        %I
-        LinVektor = reshape(I,1,[]);
+        GradientenMatrix = I2(q1:(q1+stepwith), p1:(p1+stepwith)); % nxn Umgebung %
+        LinVektor = reshape(GradientenMatrix,1,[]);
         SortedVektor = sort(LinVektor);
         for i=1:length(SortedVektor)
             foundIndex = find(LinVektor == SortedVektor(i));
             if length(foundIndex) > 1
                 foundIndexOfSortedVektor = find(SortedVektor == SortedVektor(i));
                 middlePosition = fix((foundIndexOfSortedVektor(1) + foundIndexOfSortedVektor(end)) / 2);
-                I(foundIndex) = middlePosition;
+                GradientenMatrix(foundIndex) = middlePosition;
             else
-            I(foundIndex) = i;
+            GradientenMatrix(foundIndex) = i;
             end
         end
-        GradientenMatrix(q1:(q1+stepwith), p1:(p1+stepwith)) = I;
+        RangMatrix(q1:(q1+stepwith), p1:(p1+stepwith)) = GradientenMatrix;
     end
 end
-n = padding(1);
-GradientenMatrix = GradientenMatrix(n+1:end-n, n+1:end-n); % remove padding
-GradientenMatrix = (GradientenMatrix >= threshold_rang);
+RangMatrix = RangMatrix(padding+1:end-padding, padding+1:end-padding); % remove padding
+RangMatrix = (RangMatrix >= threshold_rang);
 end
